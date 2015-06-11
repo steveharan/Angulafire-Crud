cf12550d7826cb1d0271b5ad4951d8fc7ed8be7e
Steve Haran <sharan@homeaway.co.uk>
2015-06-11 17:44:37 +0100

    controller not firing

---
+++ app/js/app.js
@@ -0,0 +1,46 @@
+(function () {
+    "use strict";
+
+    var app = angular.module('user', ['ngRoute', 'firebase', 'ui.bootstrap']);
+
+    app.value('fbURL', 'https://angularjscrudmodal.firebaseio.com/');
+    app.value('user_table', 'user');
+    app.value('country_table', 'country');
+    app.value('state_table', 'state');
+
+    app.factory('Users', function ($firebase, fbURL, user_table) {
+        return $firebase(new Firebase(fbURL + user_table));
+    });
+    app.factory('Country', function ($firebase, fbURL, country_table) {
+        return $firebase(new Firebase(fbURL + country_table));
+    });
+    app.factory('State', function ($firebase, fbURL, state_table) {
+        return $firebase(new Firebase(fbURL + state_table));
+    });
+
+    app.config([
+            "$stateProvider",
+            "$urlRouterProvider",
+            function ($stateProvider, $urlRouterProvider) {
+
+                $stateProvider
+                    .state("list", {
+                        url: "/",
+                        templateUrl: "app/views/listview.html",
+                        controller: "ListCtrl"
+                    });
+//                $urlRouterProvider.otherwise("/");
+            }]
+    );
+
+}());
+
+
+//        $routeProvider
+//            .when('/', {
+//                controller: 'ListCtrl',
+//                templateUrl: 'app/views/listview.html'
+//            })
+//            .otherwise({
+//                redirectTo: '/'
+//            });
